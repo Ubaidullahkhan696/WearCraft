4f6dd4646b07413c1ad3048d98c8fbbca27a6cc5
user.name : "ubaidullah khan <ubaidullahkhan2p@gmail.com>
2025-08-07 20:47:41 +0500

    set import problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Navbar from "./components/navbar/Navbar";
-import Shop from "./pages/Shop";
-import Product from "./pages/Product";
-import ShopCategory from "./pages/ShopCategory";
-import Cart from "./pages/Cart";
-import LoginSignup from "./pages/LoginSignup";
+import Navbar from "../src/components/navbar/Navbar";
+import Shop from "../src/pages/Shop";
+import Product from "../src/pages/Product";
+import ShopCategory from "../src/pages/ShopCategory";
+import Cart from "../src/pages/Cart";
+import LoginSignup from "../src/pages/LoginSignup";
 import LoginAready from "../src/pages/LoginAready";
 import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import menBanner from "./components/assets/banner_mens.png";
-import womenBanner from './components/assets/banner_women.png';
-import kidsBanner from  './components/assets/banner_kids.png';
+import Footer from "../src/components/Footer/Footer";
+import menBanner from "../src/components/assets/banner_mens.png";
+import womenBanner from '../src/components/assets/banner_women.png';
+import kidsBanner from  '../src/components/assets/banner_kids.png';
 
 
 
